60f15c1d88ed2576dc4ebadb506109feac478748
asankul <asankul1993@gmail.com>
2019-10-01 16:27:47 +0600

    Created models and registered admin

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from webapp.models import ToDo, Status, Type
 
-# Register your models here.
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'summary', 'created_at']
+    list_filter = ['status', 'type']
+    list_display_links = ['pk', 'summary']
+    search_fields = ['summary', 'status', 'type']
+    exclude = []
+    readonly_fields = ['created_at']
+
+
+admin.site.register(ToDo, TodoAdmin)
+admin.site.register(Status)
+admin.site.register(Type)
+
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ToDo(models.Model):
+    class Meta:
+        verbose_name_plural = "ToDo lists"
+    summary = models.CharField(max_length=200, null=False, blank=False, verbose_name='Заголовок')
+    description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='Текст')
+    status = models.ForeignKey('Status', on_delete=models.PROTECT, null=True, blank=True, verbose_name='Статус', related_name='list')
+    type = models.ForeignKey('Type', on_delete=models.PROTECT, null=True, blank=True, verbose_name='Тип', related_name='list')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+
+    def __str__(self):
+        return self.summary
+
+
+class Status(models.Model):
+    class Meta:
+        verbose_name_plural = "Statuses"
+    name = models.CharField(max_length=20, verbose_name='Статус')
+
+    def __str__(self):
+        return self.name
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=20, verbose_name='Тип')
+
+    def __str__(self):
+        return self.name
